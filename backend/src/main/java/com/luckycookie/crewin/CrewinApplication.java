6781fb9d0d60c8165473da80978bb99c781515eb
권윤하 <ellen4421@naver.com>
2024-11-04 00:30:57 +0900

    crew-in

---
+++ backend/src/main/java/com/luckycookie/crewin/CrewinApplication.java
@@ -0,0 +1,13 @@
+package com.luckycookie.crewin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luckycookie/crewin/CrewinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckycookie.crewin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
